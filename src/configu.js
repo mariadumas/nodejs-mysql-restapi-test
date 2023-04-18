e8677175abdb6b0e9cb2e8f363d45b035f6e6ff7
mariadumas <dumasmaria@gmail.com>
2023-04-18 17:26:35 -0300

    password

--- src/configu.js
+++ src/configu.js
@@ -6,7 +6,7 @@ export const PORT = process.env.PORT || 3030;
 
 export const DB_USER = process.env.DB_USER || "root";
 
-// export const DB_PASSWORD = process.env.DB_PASSWORD || null;
+export const DB_PASSWORD = process.env.DB_PASSWORD || null;
 
 export const DB_HOST = process.env.DB_HOST || "localhost";
 
